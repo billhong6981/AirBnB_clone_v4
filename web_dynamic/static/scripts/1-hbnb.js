a028754eb053ab8c0d5d1a96f33816fe0d075316
billhong6981 <bill.burton6981@gmail.com>
2019-09-28 17:44:30 +0000

    tasks 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  const checkDict = {};
+  $('LI INPUT:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      let k = $(this).attr('data-id');
+      let v = $(this).attr('data-name');
+      checkDict[k] = v;
+    }
+    if (!$(this).is(':checked')) {
+      delete checkDict[$(this).attr('data-id')]
+    }
+    let s = "";
+    const entries = Object.entries(checkDict);
+    entries.forEach(entry => {
+      entry[1] += ', ';
+      s += entry[1];
+    });
+    $('DIV.amenities h4').html(s.length > 0 ? '<i>'+ s + '</i>' : '&nbsp;');
+  });
+});
